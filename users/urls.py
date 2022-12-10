cbc30f76a893099ad8f2ac2163ba2ff46a54b150
Agnes-lica <agnes.lica@gmail.com>
2022-12-10 16:12:31 -0400

    feat: Finishing Kenzie Buster

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path("users/", views.RegisterView.as_view()),
     path("users/login/", views.LoginView.as_view()),
-    path("users/refresh/", jwt_views.TokenRefreshView.as_view())
+    path("users/refresh/", jwt_views.TokenRefreshView.as_view()),
+    path("users/<int:user_id>/", views.UserSearchView.as_view()),
 ]
